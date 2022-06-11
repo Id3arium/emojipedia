50d0973db2375d366ddce36a948158b90ff8cc6a
Alex <andres.al.campos@gmail.com>
2022-06-10 19:08:50 -0700

    adding more emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,50 +1,62 @@
 export const emojipedia = [
   {
-    emoji: "💪",
-    name: "Tense Biceps",
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not."
   },
   {
-    emoji: "🙏",
-    name: "Person With Folded Hands",
+    emoji: "😄",
+    name: " Grinning Face with Smiling Eyes",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      "A yellow face with smiling eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general happiness and good-natured amusement."
   },
   {
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "😂",
+    name: "Laughing Crying",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "A yellow face with a big grin, uplifted eyebrows, and smiling eyes, each shedding a tear from laughing so hard. Widely used to show something is funny or pleasing. "
+  },
+  /*{
+    emoji: "🙃 ",
+    name: "Upside Down Face",
+    meaning:
+      "A classic smiley, turned upside down. Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness."
+  },*/
+  {
+    emoji: "🥲 ",
+    name: "Smiling Face with Tear",
+    meaning:
+      "A yellow face with open eyes, a thin closed-mouth smile, and a single tear falling from one of its eyes. Can be used to express a wide range of emotions and expressions including gratitude, tender happiness, an attempt to look happy when actually sad, or smiling through pain. Can also be a response to something that is bittersweet."
   },
   {
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "🤔 ",
+    name: "Thinking Face",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought. Often used to question or scorn something or someone, as if saying Hmm, I don't know about that. Tone varies, including earnest, playful, puzzled, skeptical, and mocking."
   },
   {
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "😬 ",
+    name: "Grimacing Face",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "A yellow face with simple open eyes showing clenched teeth. May represent a range of negative or tense emotions, especially nervousness, embarrassment, or awkwardness (e.g., Eek!)."
   },
   {
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "😮 ",
+    name: "Face with Open Mouth",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "A yellow face with small, open eyes and a large, round mouth, slack with surprise or shock, as if saying Wow! or Oh my! May convey such feelings as awe or disbelief, often milder or more ironic in tone than 😱 Face Screaming in Fear."
   },
   {
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "🤯 ",
+    name: "Shocked Face with Exploding Head",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "A yellow face with an open mouth, the top of its head exploding in the shape of a brain-like mushroom cloud. A visual form of the expression mind blown, it may represent such emotions as shock, awe, amazement, and disbelief."
   },
   {
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "😭 ",
+    name: "Loudly Crying Face",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "A yellow face with an open mouth wailing and streams of heavy tears flowing from closed eyes. May convey inconsolable grief but also other intense feelings, such as uncontrollable laughter, pride or overwhelming joy."
   }
 ];
